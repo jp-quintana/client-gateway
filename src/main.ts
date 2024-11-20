fc31b499f3d5244ba623eec191bf86a7219901ff
jp-quintana <juanquintana1996@gmail.com>
2024-11-20 18:45:52 -0300

    test 2

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,6 @@ async function bootstrap() {
   app.useGlobalFilters(new RpcCustomExceptionFilter());
   await app.listen(envs.port);
 
-  console.log('test')
-
   logger.log(`Gateway running on port: ${envs.port}`);
 }
 bootstrap();
